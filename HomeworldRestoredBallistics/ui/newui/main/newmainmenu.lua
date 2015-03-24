9360c956c5ece945e6b20db94b68ad0373564973
Stuart Hepworth <robostuart@gmail.com>
2015-03-23 22:57:09 -0600

    Durp durp.

--- HomeworldRestoredBallistics/ui/newui/main/newmainmenu.lua
+++ HomeworldRestoredBallistics/ui/newui/main/newmainmenu.lua
@@ -247,7 +247,7 @@ NewMainMenu = {
 				sizetype = "contain",
 				
 				Text = {	
-					text = "Homeworld Restored v 0.6.2.3.1",
+					text = "Homeworld Restored v 0.6.2.3.1.1",
 					font = "ButtonFont",
 					hAlign = "Right",
 					vAlign = "Top",
