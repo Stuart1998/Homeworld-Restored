585df8697ecc2fc18661692f603b729f17b5fc49
Stuart Hepworth <robostuart@gmail.com>
2015-06-04 08:59:51 -0600

    New version.

--- HomeworldRestoredBallistics/ui/newui/main/newmainmenu.lua
+++ HomeworldRestoredBallistics/ui/newui/main/newmainmenu.lua
@@ -247,7 +247,7 @@ NewMainMenu = {
 				sizetype = "contain",
 				
 				Text = {	
-					text = "Homeworld Restored v 0.7.1",
+					text = "Homeworld Restored v 0.7.2",
 					font = "ButtonFont",
 					hAlign = "Right",
 					vAlign = "Top",
